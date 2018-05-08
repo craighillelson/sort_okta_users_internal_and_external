960e18cb099e767a39c0875f5997021f6be1d4f4
Craig Hillelson <craig.hillelson@intersection.com>
2018-05-08 12:15:07 -0400

    Add files via upload

---
+++ sort_okta_users_internal_external.py
@@ -0,0 +1,35 @@
+# prompt user
+print "What is your fully qualified domain name?"
+domain = raw_input()
+
+# import
+import csv
+
+# create lists to be populated later
+internal_users = []
+external_users = []
+
+# define write_csv function
+def write_csv(a, b):
+	with open(a, 'wb') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(['email'])
+		for email in b:
+			writer.writerow([email])
+
+# open csv and sort users into two lists (internal_users and external_users) based on their email addresses
+with open('OktaPasswordHealth.csv', 'r') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		email = row['Login']
+		if email.endswith(domain):
+			internal_users.append(email)
+		else:
+			external_users.append(email)
+
+# run function
+write_csv('internal_users.csv', internal_users)
+write_csv('external_users.csv', external_users)
+
+# update user
+print "'internal_users.csv' and 'external_users.csv' exported successfully"
